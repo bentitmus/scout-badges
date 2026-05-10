4db56e7ad8e071055b5d0bad7a15d3a424f40203
Nicola Titmus <nicola.koo@cantab.net>
2026-05-10 23:19:36 +0100

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -165,7 +165,10 @@ resource "aws_kms_key_policy" "scout_badges" {
         Sid    = "EnableRootAccountAccess"
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::${local.account_id}:root"
+          AWS = [
+            "arn:aws:iam::${local.account_id}:role/github_actions_oidc",
+            "arn:aws:iam::${local.account_id}:root"
+          ]
         }
         Action   = "kms:*"
         Resource = "*"
@@ -175,7 +178,6 @@ resource "aws_kms_key_policy" "scout_badges" {
         Effect = "Allow"
         Principal = {
           AWS = [
-            "arn:aws:iam::${local.account_id}:role/github_actions_oidc",
             "arn:aws:logs:eu-west-2:${local.account_id}:log-group:/aws/rds/*"
             #"arn:aws:iam::${local.acccount_id}:role/lambda-exec"
           ]
