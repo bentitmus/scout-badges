809c3a33fe733eb9c207f3b3dec1c2959e3ea495
Nicola Titmus <nicola.koo@cantab.net>
2026-05-11 23:54:25 +0100

    narrow kms policy

--- infra/main.tf
+++ infra/main.tf
@@ -170,8 +170,8 @@ resource "aws_kms_key_policy" "scout_badges" {
         Effect = "Allow"
         Principal = {
           AWS = [
-            #"arn:aws:iam::${local.account_id}:role/github_actions_oidc",
-            #"arn:aws:iam::${local.account_id}:root",
+            "arn:aws:iam::${local.account_id}:role/github_actions_oidc",
+            "arn:aws:iam::${local.account_id}:root"
             #"arn:aws:iam::${local.account_id}:*"
             "*"
           ]
@@ -184,7 +184,7 @@ resource "aws_kms_key_policy" "scout_badges" {
         Effect = "Allow"
         Principal = {
           AWS = [
-            #"arn:aws:logs:eu-west-2:${local.account_id}:log-group:/aws/rds/*"
+            "arn:aws:logs:eu-west-2:${local.account_id}:log-group:/aws/rds/*"
             #"arn:aws:iam::${local.acccount_id}:role/lambda-exec"
             "*"
           ]
