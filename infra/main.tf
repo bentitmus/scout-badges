14545d2d82061dd9ae1645ba8a9817cfbb3b4939
Nicola Titmus <nicola.koo@cantab.net>
2026-05-12 00:15:23 +0100

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -184,7 +184,7 @@ resource "aws_kms_key_policy" "scout_badges" {
         Effect = "Allow"
         Principal = {
           AWS = [
-            "arn:aws:logs:eu-west-2:${local.account_id}:log-group:/aws/rds/*"
+            "arn:aws:logs:eu-west-2:${local.account_id}:log-group/aws/rds/*"
             #"arn:aws:iam::${local.acccount_id}:role/lambda-exec"
             #"*"
           ]
