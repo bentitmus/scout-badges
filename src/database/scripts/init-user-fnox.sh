07e1d4ad1fd656b7060db3bdb3eb883a3d2f611b
Benjamin Titmus <ben.titmus@cantab.net>
2026-05-12 19:40:59 +0100

    fix: did not set `PGPASSWORD` locally
    `PGUSER` was made a constant so checking whether it was set doesn't work.
    Instead check whether `PGPASSWORD` has been set.

--- src/database/scripts/init-user-fnox.sh
+++ src/database/scripts/init-user-fnox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z ${PGUSER+x} ]; then
+if [ -z ${PGPASSWORD+x} ]; then
   # For the local machine use a randomly generated password
   openssl rand -hex 100 | fnox set --config fnox.local.toml --profile default --provider age PGPASSWORD
 fi
