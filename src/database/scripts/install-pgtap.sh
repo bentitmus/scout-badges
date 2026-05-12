832bcc159fa759cb6d11f271a0593833d6ee2575
Benjamin Titmus <ben.titmus@cantab.net>
2026-05-12 22:42:10 +0100

    fix: GitHub database lint workflow failures

--- src/database/scripts/install-pgtap.sh
+++ src/database/scripts/install-pgtap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 export PGUSER=postgres
+export PGHOST=localhost
+export PGPORT=5432
+export PGDATA=$1
 curl https://api.pgxn.org/dist/pgtap/1.3.4/pgtap-1.3.4.zip --output pgtap-1.3.4.zip
 unzip pgtap-1.3.4.zip
 cd pgtap-1.3.4
