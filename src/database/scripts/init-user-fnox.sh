bdd1ea46d33178f5c386235d35e480e424dbe820
Benjamin Titmus <ben.titmus@cantab.net>
2026-05-12 18:26:53 +0100

    refactor: fix the username for local machine Postgres

--- src/database/scripts/init-user-fnox.sh
+++ src/database/scripts/init-user-fnox.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 if [ -z ${PGUSER+x} ]; then
-  # For the local machine use a default `dbadmin` username and a randomly
-  # generated password
-  echo dbadmin | fnox set --config fnox.local.toml --profile default --provider age PGUSER
+  # For the local machine use a randomly generated password
   openssl rand -hex 100 | fnox set --config fnox.local.toml --profile default --provider age PGPASSWORD
 fi
