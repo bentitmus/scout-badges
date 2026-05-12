23ef0a4ad0f8c9733bcc56de03bd50d117cbfa0e
Benjamin Titmus <ben.titmus@cantab.net>
2026-05-12 19:42:29 +0100

    feat: add descriptions for `fnox` passwords

--- infra/init-vars.sh
+++ infra/init-vars.sh
@@ -10,6 +10,11 @@ rds_port=$(echo $rds_endpoint | sed -e 's/.*://')
 rds_admin=$(aws secretsmanager get-secret-value --secret-id $aws_rds_sm_key | jq ".SecretString | fromjson")
 # Set the username, password, and database URL in the `src/database` directory
 pushd $(git rev-parse --show-toplevel)/src/database
+fnox set --config fnox.local.toml --description "Username of Postgres admin account" --profile $MISE_ENV --provider age PGUSER
+fnox set --config fnox.local.toml --description "Password for Postgres admin account" --profile $MISE_ENV --provider age PGPASSWORD
+fnox set --config fnox.local.toml --description "Address of Postgres database server" --profile $MISE_ENV --provider age PGHOST
+fnox set --config fnox.local.toml --description "Port number of Postgres database server" --profile $MISE_ENV --provider age PGPORT
+fnox set --config fnox.local.toml --description "Database name" --profile $MISE_ENV --provider age PGDATABASE
 echo $rds_admin | jq '.username' --raw-output | fnox set --config fnox.local.toml --profile $MISE_ENV --provider age PGUSER
 echo $rds_admin | jq '.password' --raw-output | fnox set --config fnox.local.toml --profile $MISE_ENV --provider age PGPASSWORD
 echo $rds_host | fnox set --config fnox.local.toml --profile $MISE_ENV --provider age PGHOST
--- src/database/scripts/init-user-fnox.sh
+++ src/database/scripts/init-user-fnox.sh
@@ -2,5 +2,6 @@
 
 if [ -z ${PGPASSWORD+x} ]; then
   # For the local machine use a randomly generated password
+  fnox set --config fnox.local.toml --description "Password for Postgres admin account" --profile default --provider age PGPASSWORD
   openssl rand -hex 100 | fnox set --config fnox.local.toml --profile default --provider age PGPASSWORD
 fi
